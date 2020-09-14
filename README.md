79c12dbc10e3071288c96b1fd1930b43e79334d9
Fábio Veiga <71238693+FabioV37ga@users.noreply.github.com>
2020-09-14 00:36:15 -0300

    Added price variable

--- README.md
+++
@@ -1,9 +0,0 @@
-# PTBR-E-Commerce-color-selector-js
-(PT-BR) Trata um menu de cores para um produto.
-(EN) Treat a product's color menu.
-
-→ https://prnt.sc/ugqnrf
-→ https://prnt.sc/ugqofc
-
-(PT-BR) Só isso mesmo.
-(EN) not much to explain.
---
+++ src/js/botao.js
@@ -0,0 +1,40 @@
+; (function () {
+
+
+    const buttons = document.querySelectorAll('.botao')
+
+    for (let i = 0; i < buttons.length; i++) {
+
+        const button = buttons[i]
+
+        button.addEventListener('click', function () {
+
+            const displayColor = document.querySelector('.display__color');
+            const buttons = document.querySelectorAll('.botao');
+            const camiseta = document.querySelector('#camiseta')
+
+            for (let i = 0; i < buttons.length; i++) {
+
+                const button = buttons[i]
+
+                if (button === this) {
+                    button.classList.add('__selected');
+                    displayColor.textContent = `cor selecionada: ${button.name}`;
+
+                    camiseta.src = `src/img/${button.name}.png`
+                }
+                else {
+                    button.classList.remove('__selected')
+                }
+            }
+
+        })
+    }
+    const comprar = document.querySelector('.send__button')
+    comprar.addEventListener('click', function () {
+        const itemSelecionado = document.querySelector('.__selected')
+        console.log(`Você escolheu o produto ${itemSelecionado.name}, por R$${itemSelecionado.value}.00`)
+    })
+})();
+
+
