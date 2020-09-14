8e5b190ae00335b2827f9dfd8ef7d6f38c67b532
Fábio Veiga <71238693+FabioV37ga@users.noreply.github.com>
2020-09-14 14:52:04 -0300

    09-14-2020 14:52

--- src/js/botao.js
+++ src/js/botao.js
@@ -18,9 +18,7 @@
 
                 if (button === this) {
                     button.classList.add('__selected');
-
                     displayColor.textContent = `cor selecionada: ${button.name}`;
-
                     camiseta.src = `src/img/${button.name}.png`
                 }
                 else {
@@ -31,13 +29,29 @@
         })
     }
 
+    // Buy button (not that important)
+
     const comprar = document.querySelector('.send__button')
 
     comprar.addEventListener('click', function () {
 
-        const itemSelecionado = document.querySelector('.__selected')
-
+        const itemSelecionado = document.querySelector('.__selected');
+        const product = document.querySelector('.produto');
+        const comprado = document.querySelector('.sucesso');
+        const sucessoTexto = document.querySelector('.sucesso__texto')
+        
         console.log(`Você escolheu o produto ${itemSelecionado.name}, por R$${itemSelecionado.value}.00`)
+
+        // Success animation
+
+        product.classList.add('fade-out')
+
+        product.addEventListener('transitionend', function(){
+            product.remove()
+            comprado.classList.add('fade-in')
+            comprado.style.display = 'flex'
+            sucessoTexto.textContent = `Produto ${itemSelecionado.name} comprado por R$${itemSelecionado.value}` 
+        })
     })
 })();
 
